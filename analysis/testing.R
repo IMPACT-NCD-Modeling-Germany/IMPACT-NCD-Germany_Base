0a335629ff808a8b80a397cd271de6fea66bb5a2
kalleEF <kalleEF@users.noreply.github.com>
2025-10-01 09:36:17 +0000

    Testing commit inside container.

--- analysis/testing.R
+++ analysis/testing.R
@@ -3,6 +3,9 @@
 library(data.table)
 
 n <- 100000
-dt <- data.table(runif(n), rnorm(n))
+dt <- data.table(runif(n), rnorm(n), rgamma(n, 0.5))
 
 fwrite(dt, "./outputs/testing_rsync.csv")
+
+
+## Additional test analysis
